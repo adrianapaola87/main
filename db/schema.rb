ef79372e674560bfe5cccaa95480d183b52ef09c
adri <adri_8908@hotmail.com>
2017-06-28 12:09:27 +0200

    trabajofinal

--- db/schema.rb
+++ db/schema.rb
@@ -121,8 +121,6 @@ ActiveRecord::Schema.define(version: 20170625085838) do
     t.integer  "marketing_mailer"
     t.date     "marketing_mailer_date"
     t.string   "locale"
-    t.string   "username"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
