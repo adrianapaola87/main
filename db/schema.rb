de6347aa8c3648bdb39b9d4f742ee9d9e1fd299d
adri <adri_8908@hotmail.com>
2017-06-28 23:26:52 +0200

    trabajo final de curso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170625085838) do
+ActiveRecord::Schema.define(version: 20170628202017) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20170625085838) do
     t.string   "description"
     t.text     "descripcion"
     t.integer  "price"
+    t.string   "username"
   end
 
   create_table "settings", force: :cascade do |t|
